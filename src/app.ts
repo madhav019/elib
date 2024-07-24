1b7f265fdc0c8c17794ad3580458d4f98cd15fc4
madhav019 <madhavchawla19@gmail.com>
2024-07-25 00:16:47 +0530

    add registor user validation

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,19 @@ import express, { NextFunction, Request, Response } from "express";
 import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
-
+app.use(express.json());
 // Routes
 
 app.get("/", (req, res, next) => {
-  const error = createHttpError(400, "Something went wrong");
-
-  throw error;
-
   res.json({
     message: "Welcome to elib apis",
   });
 });
+
+app.use("/api/users/", userRouter);
 
 app.use(globalErrorHandler);
 
